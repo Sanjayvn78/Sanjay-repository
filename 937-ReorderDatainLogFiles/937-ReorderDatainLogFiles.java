a8f39e97953f612e49d6604f17d9bb3335848e93
Sanjayvn78 <sanjaysundravadivel06@gmail.com>
2026-09-11 22:06:58 +0530

    File updated at 9/11/2026, 10:07:00 PM

--- 937-ReorderDatainLogFiles/937-ReorderDatainLogFiles.java
+++ 937-ReorderDatainLogFiles/937-ReorderDatainLogFiles.java
@@ -1,20 +1,19 @@
-// Last updated: 9/11/2026, 10:04:32 PM
+// Last updated: 9/11/2026, 10:07:00 PM
 1class Solution {
-2    public int mctFromLeafValues(int[] arr) {
-3         Stack<Integer>stack = new Stack<>();
-4         stack.push(Integer.MAX_VALUE);
-5         int ans = 0;
-6         for(int nums:arr){
-7            while(stack.peek()<=nums){
-8             int top = stack.pop();
-9             ans+=top*Math.min(stack.peek(),nums);
-10            }
-11            stack.push(nums);
-12         }
-13         while(stack.size()>2){
-14            ans+=stack.pop()*stack.peek();
-15         }   
-16         return ans;
-17    }  
-18
-19}
+2    public boolean isMonotonic(int[] nums) {
+3        boolean isIncreasing = true;
+4        boolean isDecreasing = true;
+5        for (int i =1; i<nums.length; i++){
+6            if (nums[i]>nums[i-1]){
+7                isDecreasing = false;
+8            }
+9            else if (nums[i]<nums[i-1]){
+10                isIncreasing = false;
+11            }
+12            if(!isIncreasing && !isDecreasing){
+13                return false;
+14            }
+15        }
+16        return true;
+17    }
+18}
