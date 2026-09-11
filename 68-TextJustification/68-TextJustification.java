bab7d64b08c553f64241e1ee1a043f47e12bb69a
Sanjayvn78 <sanjaysundravadivel06@gmail.com>
2026-09-11 21:56:39 +0530

    File updated at 9/11/2026, 9:56:41 PM

--- 68-TextJustification/68-TextJustification.java
+++ 68-TextJustification/68-TextJustification.java
@@ -1,18 +1,8 @@
-// Last updated: 9/11/2026, 9:55:44 PM
+// Last updated: 9/11/2026, 9:56:41 PM
 1class Solution {
-2    public int findPeakElement(int[] nums) {
-3        int l = 0,
-4            r = nums.length-1;
-5
-6        while(l < r) {
-7            int mid = l + (r - l) / 2;
-8
-9            if(nums[mid] > nums[mid+1]) {
-10                r = mid;
-11            } else {
-12                l = mid + 1;
-13            }
-14        }
-15        return l;
-16    }
-17}
+2    public int majorityElement(int[] nums) {
+3        Arrays.sort(nums);
+4        int n = nums.length;
+5        return nums[n/2];
+6    }
+7}
